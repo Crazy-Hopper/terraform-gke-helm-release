38a51745c2207e4ead4ebc1f8218802387440f79
Eugene Paskevich <evgeniy.paskevich@tranzzo.com>
2023-11-11 21:45:39 +0100

    Added GKE auth.

---
+++ providers.tf
@@ -0,0 +1,20 @@
+provider google {
+  project = var.google_project
+  region  = var.google_region
+}
+
+data google_client_config default {}
+
+provider kubernetes {
+  host                   = "https://${var.cluster_endpoint}"
+  cluster_ca_certificate = var.cluster_ca_certificate
+  token                  = data.google_client_config.default.access_token
+}
+
+provider helm {
+  kubernetes {
+    host                   = "https://${var.cluster_endpoint}"
+    cluster_ca_certificate = var.cluster_ca_certificate
+    token                  = data.google_client_config.default.access_token
+  }
+}
